85733b58f93001b9054c0b05d876f986232822a9
Prakash Mandal <pm1447616@gmail.com>
2022-02-15 21:45:57 +0530

    Add project files.

---
+++ MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,23 @@
+﻿using chat_app.MVVM.Core;
+using chat_app.net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chat_app.MVVM.ViewModel
+{
+     class MainViewModel
+    {
+        public RelayCommand ConnectToServerCommand { get; set; }
+
+        private Server _server;
+        public  MainViewModel()
+        {
+            _server = new Server();
+            ConnectToServerCommand = new RelayCommand(o => _server.ConnectToServer());
+
+        }
+    }
+}
